a14f5c9f4a01fec0e4180675b577c67fa5b38c8d
Lucky-bro4 <shuiskov2017@gmail.com>
2024-07-02 03:13:53 +0300

    ok

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize')
 module.exports = new Sequelize(
     'switchdb',
     'switchadmin',
-    'qwerty',
+    'qwerty1357924680songP',
     {
         host: 'amvera-lucky-bro4-cnpg-switch-db-rw',
         port: '5432',
